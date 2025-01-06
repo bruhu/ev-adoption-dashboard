d9cb34e38918aac434dfb70ec70dc417ce6bdd23
bru <brugera.herrero@gmail.com>
2025-01-06 15:39:11 +0100

    add streamlit main script

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from utils.data_loader import load_sales_data  # Importing from utils.py
+
+# load sales data
+sales_df = load_sales_data()
+
+# Streamlit stuff
+st.title('EV Sales Data')
+st.write(sales_df)
